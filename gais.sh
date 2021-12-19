66c6b2e42d55fa945dfe61e75dadae5e3219bd8b
dschense <jens.walser@gmail.com>
2021-12-19 11:11:32 +0100

    comments
    commented configuration steps

--- gais.sh
+++ gais.sh
@@ -6,14 +6,15 @@
 
 # Intro text
 echo "
- ██████╗  █████╗ ██╗███████╗
-██╔════╝ ██╔══██╗██║██╔════╝
-██║  ███╗███████║██║███████╗
-██║   ██║██╔══██║██║╚════██║
-╚██████╔╝██║  ██║██║███████║
- ╚═════╝ ╚═╝  ╚═╝╚═╝╚══════╝
-                            
-Gianmarco's Arch Installer Script
+ 
+██████╗░░██████╗░█████╗░██╗░░██╗███████╗███╗░░██╗░██████╗███████╗
+██╔══██╗██╔════╝██╔══██╗██║░░██║██╔════╝████╗░██║██╔════╝██╔════╝
+██║░░██║╚█████╗░██║░░╚═╝███████║█████╗░░██╔██╗██║╚█████╗░█████╗░░
+██║░░██║░╚═══██╗██║░░██╗██╔══██║██╔══╝░░██║╚████║░╚═══██╗██╔══╝░░
+██████╔╝██████╔╝╚█████╔╝██║░░██║███████╗██║░╚███║██████╔╝███████╗
+╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝╚═════╝░╚══════╝
+
+dschense's Arch Installer Script modifyed from
 (C) 2021 Gianmarco Gargiulo - GPL v3
 
 WARNING: this script is experimental.
@@ -30,6 +31,7 @@ sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 loadkeys de
 timedatectl set-ntp true
 
+# Partitioning with cfdisk
 lsblk
 echo "You'll be asked for where to install your OS. Use cfdisk to partition the drive.
 These are some reccommended partition schemes.
@@ -47,11 +49,15 @@ Target drive: "
 read drive
 cfdisk $drive
 
+# Format and mount partitions
 lsblk
+
+# Root
 echo "Target root partition (MUST BE FORMATTED so make sure you have nothing important on it): "
 read partition
 mkfs.ext4 $partition
 
+# Boot
 read -p "Did you make an EFI partition for UEFI? [y/n] " answer
 if [[ $answer = y ]] ; then
   echo "Target EFI partition: "
@@ -63,6 +69,7 @@ else
   mkfs.ext4 $legacyboot
 fi
 
+# Swap
 read -p "Did you make a swap partition? [y/n] " answer
 if [[ $answer = y ]] ; then
   echo "Target swap partition: "
@@ -70,15 +77,18 @@ if [[ $answer = y ]] ; then
   swapon $swappartition
 fi
 
+# Mounting
 mount $partition /mnt
 mkdir /mnt/boot
 mount $legacyboot /mnt/boot
+
+# Install Base System
 pacstrap /mnt base base-devel linux-zen linux-zen-headers
 genfstab -U /mnt >> /mnt/etc/fstab
 
-sed '1,/^# Configuration$/d' gais.sh > /mnt/gais_part2.sh
-chmod +x /mnt/gais_part2.sh
-arch-chroot /mnt ./gais_part2.sh
+sed '1,/^# Configuration$/d' arch.sh > /mnt/arch_part2.sh
+chmod +x /mnt/arch_part2.sh
+arch-chroot /mnt ./arch_part2.sh
 exit
 
 
@@ -88,14 +98,17 @@ pacman -Sy
 pacman -S --noconfirm sed
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 
+# TimeZone
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
 
+# Locale
 echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=de_DE.UTF-8" > /etc/locale.conf
 echo "KEYMAP=de" > /etc/vconsole.conf
 
+# Hostname
 echo "Type a hostname for your system: "
 read hostname
 echo $hostname > /etc/hostname
@@ -103,10 +116,15 @@ echo "127.0.0.1       localhost" >> /etc/hosts
 echo "::1             localhost" >> /etc/hosts
 echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
 
+# Initial ramdisk
 mkinitcpio -P
+
+# Set root PW
 echo "You will now be asked to input a password for the root user."
 passwd
 pacman --noconfirm -S grub efibootmgr os-prober
+
+# Setup GRUB
 read -p "Did you make an EFI partition for UEFI? [y/n] " answer
 if [[ $answer = y ]] ; then
 lsblk
@@ -117,25 +135,34 @@ mount $efipartition /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 else
 lsblk
-echo "Enter boot drive for MBR: "
+echo "Enter boot drive for MBR (/dev/sda NOT /dev/sda1): "
 read bootdrive
 grub-install --target=i386-pc $bootdrive
 fi
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Install packages
 pacman -S --noconfirm xorg-server sddm plasma akregator ark audiocd-kio dolphin dolphin-plugins elisa ffmpegthumbs filelight gwenview k3b kaddressbook kate kcalc kcharselect kcolorchooser kdeconnect kdegraphics-thumbnailers kdenetwork-filesharing kdenlive kdialog kio-extras kleopatra kmail konsole konversation krdc krfb krita ktorrent markdownpart okular partitionmanager spectacle svgpart yakuake
 pacman -R pulseaudio
 pacman -S mpv pipewire-pulse zsh rsync pavucontrol-qt opendoas
 
+# Enable NetworkManager
 systemctl enable NetworkManager.service
+
+# Create local user with sudo rights
 echo "permit persist keepenv :wheel as root" > /etc/doas.conf
 echo "Create your own user account. It will have administrative privileges (wheel)."
 echo "Username: "
 read username
 useradd -m -G wheel -s /bin/zsh $username
 passwd $username
+
+# Install packetmanager YAY
 runuser -l $username -c 'cd && mkdir Git && cd Git && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && yay -S --noconfirm librewolf-bin librewolf-extension-dark-reader librewolf-extension-localcdn librewolf-extension-plasma-integration librewolf-extension-return-youtube-dislike-git librewolf-ublock-origin opendoas-sudo'
+
+# unmount installed filesystem
+umount -R /mnt
 
 echo "
 ----------------------------------------------------------------------------------
