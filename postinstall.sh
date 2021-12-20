bfd38da0a1c87bd3823094794ae26313bcb9c481
dschense <jens.walser@gmail.com>
2021-12-20 07:33:50 +0100

    commented

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #postinstall
 
+#manual link doas to sudo
 doas mv /usr/bin/sudo /usr/bin/sudo.BAK
 doas ln -s $(which doas) /usr/bin/sudo
 
+#install correct opendoas link
 mkdir Git $$ cd Git && git clone https://aur.archlinux.org/opendoas-sudo.git && cd opendoas-sudo && makepkg -si
-doas pacman -S --needed base-devel
+#install paru pacman wrapper
 cd ~/Git && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si 
  
